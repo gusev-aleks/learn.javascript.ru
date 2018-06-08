b9128c8c793665934c7c04c90223312fc49a7b5c
Alex G <aleks.v.gusev@gmail.com>
2018-06-08 18:59:56 +0300

    - Add template for 02-12 lesson; - Testing "if" outside span with "!" and without.

---
+++ 02-13/02-13-02/LOgic.js
@@ -0,0 +1,7 @@
+var age=10;
+
+if(age < 14 || age >90){
+    alert ("true");
+} else {
+    alert ("false");
+}
---
+++ 02-13/02-13-02/LOgicWith!.js
@@ -0,0 +1,7 @@
+var age= 20;
+
+if(!(age >=14 && age <=90)){
+    alert("true");
+} else {
+    alert("false");
+}

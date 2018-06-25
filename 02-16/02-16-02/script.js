539cc99023bfae79dca98263b5d35e14d373b848
Alex G <aleks.v.gusev@gmail.com>
2018-06-25 16:08:06 +0300

    - Add template for 02-16 lesson; - Rewrite if by switch.

---
+++ 02-16/02-16-02/script.js
@@ -0,0 +1,14 @@
+"use strict";
+var a= prompt("A?");
+    switch (a){
+        case "0" :
+            alert("0");
+            break;
+        case "1" :
+            alert("1");
+            break;
+        case "2":
+        case "3":
+            alert("2,3");
+            break;
+}

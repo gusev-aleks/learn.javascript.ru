13467548a8dbe058f6441317655b29a4a50d37da
Alex G <aleks.v.gusev@gmail.com>
2018-06-29 18:42:30 +0300

    - Add template for 02-17 lesson; - Write function, that erect numbers to a power.

---
+++ 02-17/02-17-01/script.js
@@ -0,0 +1,17 @@
+function pow(x, n) {
+    var result = x;
+
+    for (var i = 1; i < n; i++) {
+        result *= x;
+    }
+    return result;
+}
+
+var x = prompt("x?", '');
+var n = prompt("n?", '');
+
+if (n <= 1) {
+    alert( "Введите степень, больше 1");
+} else {
+    alert( pow(x,n) );
+}
